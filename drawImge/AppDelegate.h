6b96d304499ad112cb5f55c9ec8172ce33dc4d6e
Sunsline <hydom@HdtekiMac-mini.local>
2017-01-20 15:59:41 +0800

    drawImge
    UIImge设置圆角，两张图片合并

---
+++ drawImge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drawImge
+//
+//  Created by hydom on 2017/1/17.
+//  Copyright © 2017年 hydom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ drawImge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  drawImge
+//
+//  Created by hydom on 2017/1/17.
+//  Copyright © 2017年 hydom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
